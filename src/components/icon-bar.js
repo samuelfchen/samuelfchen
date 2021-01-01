f4d47ce4f866f4fc11925df6adf4ac327702d9a2
Samuel Chen <33302418+s-f-chen@users.noreply.github.com>
2021-01-01 15:47:44 +1100

    transfer

--- src/components/icon-bar.js
+++ src/components/icon-bar.js
@@ -9,10 +9,10 @@ import resumeIcon from "../images/resume-icon.svg"
 const IconBar = () => (
     <div> 
         <nav>
-            <a href="https://github.com/s-f-chen" target="_blank"><img src={githubIcon} alt="github"/></a>
-            <a href="mailto:samuel.f.chen@gmail.com" target="_blank"><img src={emailIcon} alt="email"/></a>
-            <a href="https://www.linkedin.com/in/samuel-chen-849b41177/" target="_blank"><img src={linkedinIcon} alt="linkedIn"/></a>
-            <Link to="/resume" target="_blank"><img src={resumeIcon} alt="resume"/></Link>
+            <a href="https://github.com/s-f-chen" target="_blank" rel="noreferrer"><img src={githubIcon} alt="github"/></a>
+            <a href="mailto:samuel.f.chen@gmail.com" target="_blank" rel="noreferrer"><img src={emailIcon} alt="email"/></a>
+            <a href="https://www.linkedin.com/in/samuel-chen-849b41177/" target="_blank" rel="noreferrer"><img src={linkedinIcon} alt="linkedIn"/></a>
+            <Link to="/resume" target="_blank" rel="noreferrer"><img src={resumeIcon} alt="resume"/></Link>
         </nav>
     </div>
 )
