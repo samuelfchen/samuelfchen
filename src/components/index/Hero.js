d227707b1bfcc7bc4704c61c476e3df49055287a
s-f-chen <33302418+s-f-chen@users.noreply.github.com>
2021-02-22 02:51:48 +1100

    trying to fix scroll issue

--- src/components/index/Hero.js
+++ src/components/index/Hero.js
@@ -1,5 +1,8 @@
 import React, {useEffect, useRef} from "react"
 import IconBar from "./IconBar.js"
+import ImageArt from "./ImageArt.js"
+
+import { TweenLite } from 'gsap'
 
 import HeroWrapper from '../../styles/index/HeroStyles'
 
@@ -8,11 +11,31 @@ import Mesh from './Mesh'
 import down from '../../images/icons/down.svg'
 
 const Hero = () => {
+    let imageItem = useRef(null);
+    let heroItem = useRef(null);
+    
+    useEffect(() => {
+        TweenLite.fromTo(heroItem, 1, {
+            y: 40,
+            opacity: 0
+        }, {
+            y:0,
+            opacity: 1,
+            delay: 0.5
+        })
+
+        // TweenLite.fromTo(imageItem, 1, {
+        //     opacity: 0
+        // }, {
+        //     opacity: 1,
+        //     delay: 1.5
+        // })
+    })
 
     return (
         <HeroWrapper>
             <div className="hero-content">
-                <div className="description" >
+                <div className="description" ref={el => {heroItem = el}}>
                     <div className="name">
                         <p>samuel <br className="rwd-break"/>chen</p>
                     </div>
